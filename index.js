62e68536b48981191b1ed4519b4a9658a7580577
Kazuhiko Okumura <engkokmr@gmail.com>
2021-10-02 20:23:39 +0900

    アプリケーションでMongoDBに接続するようにContext設定を追加する

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 const {ApolloServer} = require(`apollo-server-express`)
 const express = require(`express`)
 const expressPlayground = require(`graphql-playground-middleware-express`).default
+const {MongoClient} = require(`mongodb`)
 const {readFileSync} = require(`fs`)
 
 const typeDefs = readFileSync(`./typeDefs.graphql`, `UTF-8`)
 const resolvers = require(`./resolvers`)
 
-const app = express()
-const server = new ApolloServer({typeDefs, resolvers})
+require(`dotenv`).config()
 
-server.applyMiddleware({app})
+async function start() {
 
-app.get(`/`, (req, res) => res.end(`Welcome to the PhotoShare API`))
-app.get(`/playground`, expressPlayground({endpoint: `/graphql`}))
+  const app = express()
 
-app.listen({port: 4000}, () =>
-  console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`)
-)
+  const MONGO_DB = process.env.DB_HOST
+
+  const client = await MongoClient.connect(MONGO_DB, {useNewUrlParser: true})
+
+  const db = client.db()
+
+  const context = {db}
+
+  const server = new ApolloServer({typeDefs, resolvers, context})
+
+  server.applyMiddleware({app})
+
+  app.get(`/`, (req, res) => res.end(`Welcome to the PhotoShare API`))
+  app.get(`/playground`, expressPlayground({endpoint: `/graphql`}))
+
+  app.listen({port: 4000}, () =>
+    console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`)
+  )
+}
+
+start()
