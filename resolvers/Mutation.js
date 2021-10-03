3b908cb724adb3f1f54e6487141367c87cc8c9ed
Kazuhiko Okumura <engkokmr@gmail.com>
2021-10-03 11:00:51 +0900

    GitHub認可でログインユーザーのトークンを取得するまでを追加 - GitHubミューテーションを実行して認可コードからアクセストークンを取得する - 認可コードを取得した時のログインユーザー情報をMongoDBに登録

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,3 +1,4 @@
+const {authorizeWithGithub} = require('../lib')
 let {_id, photos} = require('./variables')
 
 module.exports = {
@@ -9,5 +10,38 @@ module.exports = {
     }
     photos.push(newPhoto)
     return newPhoto
+  },
+
+  async githubAuth(parent, {code}, {db}) {
+
+    let {
+      message,
+      access_token,
+      avatar_url,
+      login,
+      name
+    } = await authorizeWithGithub({
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      code
+    })
+
+    if (message) {
+      throw new Error(message)
+    }
+
+    let latestUserInfo = {
+      name,
+      githubLogin: login,
+      githubToken: access_token,
+      avatar: avatar_url
+    }
+
+    await db
+      .collection('users')
+      .replaceOne({githubLogin: login}, latestUserInfo, {upsert: true})
+
+    const user = latestUserInfo
+    return {user, token: access_token}
   }
 }
