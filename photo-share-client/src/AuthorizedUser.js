b1895efc36f1558b877e0ad63737af1a50366e14
Kazuhiko Okumura <engkokmr@gmail.com>
2021-10-09 15:07:29 +0900

    GitHubのOAuthを用いたclient_codeを取得する機能を実装しました。 - GitHubのOAuthを呼び出すボタンを追加 - OAuthで認可されたコードをアラートで出力する

---
+++ photo-share-client/src/AuthorizedUser.js
@@ -0,0 +1,31 @@
+import React, {Component} from "react";
+import {withRouter} from "react-router-dom"
+
+class AuthorizedUser extends Component {
+
+  state = {signingIn: false}
+
+  componentDidMount() {
+    if (window.location.search.match(/code=/)) {
+      this.setState({signingIn: true})
+      const code = window.location.search.replace("?code=", "")
+      alert(code)
+      this.props.history.replace("/")
+    }
+  }
+
+  requestCode() {
+    let clientID = process.env.REACT_APP_CLIENT_ID
+    window.location = `https://github.com/login/oauth/authorize?client_id=${clientID}&scope=user`
+  }
+
+  render() {
+    return (
+      <button onClick={this.requestCode} disabled={this.state.signingIn}>
+        Sign In with GitHub
+      </button>
+    )
+  }
+}
+
+export default withRouter(AuthorizedUser)
