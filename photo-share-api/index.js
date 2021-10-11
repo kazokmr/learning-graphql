4a1daf85fe80623ccbc616bfe6c5e6e4c376f288
Kazuhiko Okumura <engkokmr@gmail.com>
2021-10-12 05:53:41 +0900

    Queryの制限を設定しました - 深さは最大５としています。 - 複雑度の上限は1000としました。

--- photo-share-api/index.js
+++ photo-share-api/index.js
@@ -4,11 +4,13 @@ const {createServer} = require('http')
 const {ApolloServer, PubSub} = require('apollo-server-express')
 const {MongoClient} = require('mongodb')
 const {readFileSync} = require('fs')
-require('dotenv').config()
-
-const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8')
 const resolvers = require('./resolvers')
 const path = require("path");
+const depthLimit = require("graphql-depth-limit");
+const {createComplexityLimitRule} = require("graphql-validation-complexity");
+
+require('dotenv').config()
+const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8');
 
 async function start() {
 
@@ -21,6 +23,12 @@ async function start() {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    validationRules: [
+      depthLimit(5),
+      createComplexityLimitRule(1000, {
+        onCost: cost => console.log('query cost: ', cost)
+      })
+    ],
     context: async ({req, connection}) => {
       const githubToken = req ? req.headers.authorization : connection.context.Authorization
       const currentUser = await db.collection('users').findOne({githubToken})
